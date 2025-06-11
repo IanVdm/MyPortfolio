ac3cb0a540bc92309a252264be05373b59e8a23c
Ian van der Merwe <ianvdm2001@gmail.com>
2025-06-11 16:29:37 +0200

    Implemented footer section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,28 +6,30 @@ import { SkillsSection } from "../components/SkillsSection"
 import { StarBackground } from "../components/StarBackground"
 import { ProjectSection } from "../components/ProjectSection"
 import { ContactSection } from "../components/ContactSection"
+import { Footer } from "../components/Footer"
 
 export const Home = () => {
-  return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
 
-    {/* Theme Toggle */}
-    <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
 
-    {/* Background Effects */}
-    <StarBackground />
+      {/* Navbar */}
+      <NavBar />
 
-    {/* Navbar */}
-    <NavBar />
+      {/* Main Content */}
+      <main>
+        <HeroSection />
+        <SkillsSection />
+        <ProjectSection />
+        <ContactSection />
+      </main>
 
-    {/* Main Content */}
-    <main>
-      <HeroSection />
-      <SkillsSection />
-      <ProjectSection />
-      <ContactSection />
-    </main>
-
-    {/* Footer */}
-
-  </div>
+      {/* Footer */}
+      <Footer />
+    </div>
+  )
 }
